5392b62a3601720c16731257ab71e49a4fe02264
priya <priyapardeshi610@gmail.com>
2024-09-03 18:43:28 +0530

    firstcommit

---
+++ project/app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from  .models import*
+from .forms import*
+
+def home(request):
+    form = ItemInfoForm()
+    if request.method =="POST":
+        form = ItemInfoForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+    data = ItemInfo.objects.all()
+    return render(request,'home.html',{'form' :form,'data':data})        
